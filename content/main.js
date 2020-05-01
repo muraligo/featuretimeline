5b90b2793987c99e55a79de9b76fb57f02ffb483
muraligo <murali80027@yahoo.com>
2020-05-01 10:15:08 -0600

    split JSON

--- content/main.js
+++ content/main.js
@@ -1,6 +1,10 @@
 import { Task, TaskSet } from './tasks.js';
 
-var jsonstr = '[{"name":"ElabFeat","row":1,"kind": "SERVICE","tasks":[{"name":"ElabFeatI","textlines":["Elaborate", "Feature"]}]}, {"name": "CreateSPR0", "row": 1, "kind": "ARCHITECTURE", "tasks": [ { "name": "CreateSPR1", "textlines": ["Create", "SPR"] } ], "predecessors": ["ElabFeat"]}, {"name": "CreateDoc0", "row": 0, "kind": "SERVICE", "tasks": [ { "name": "CreateDoc1", "textlines": ["Create Docs", "(architecture", "feature defn", "jump page)"] } ], "predecessors": ["CreateSPR0"]}, {"name": "ECAR", "row": 0, "kind": "SECURITY", "timeline": "2 weeks", "tasks": [ { "name": "SchedECAR", "textlines": ["Schedule", "ECAR", "Review"] }, { "name": "PerfECAR", "textlines": ["Perform", "ECAR", "Review"] } ], "predecessors": ["CreateDoc0"]} ]';
+var jsonstr = '[{"name":"ElabFeat","row":1,"kind": "SERVICE","tasks":[{"name":"ElabFeatI","textlines":["Elaborate", "Feature"]}]}, ' + 
+            '{"name": "CreateSPR0", "row": 1, "kind": "ARCHITECTURE", "tasks": [ { "name": "CreateSPR1", "textlines": ["Create", "SPR"] } ], "predecessors": ["ElabFeat"]}, ' + 
+            '{"name": "CreateDoc0", "row": 0, "kind": "SERVICE", "tasks": [ { "name": "CreateDoc1", "textlines": ["Create Docs", "(architecture", "feature defn", "jump page)"] } ], "predecessors": ["CreateSPR0"]}, ' + 
+            '{"name": "ECAR", "row": 0, "kind": "SECURITY", "timeline": "2 weeks", "tasks": [ { "name": "SchedECAR", "textlines": ["Schedule", "ECAR", "Review"] }, { "name": "PerfECAR", "textlines": ["Perform", "ECAR", "Review"] } ], "predecessors": ["CreateDoc0"]} ' + 
+            ']';
 
 var taskgraph = [];
 
